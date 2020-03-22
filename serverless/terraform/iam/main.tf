322871526f80f96e51edf392f4fd42625b5b0f49
Mateusz Biel <matbiel@gmail.com>
2020-03-22 20:26:10 +0100

    add policy to role

--- serverless/terraform/iam/main.tf
+++ serverless/terraform/iam/main.tf
@@ -13,7 +13,11 @@ resource "aws_iam_role" "lambda_iam_role" {
   assume_role_policy = "${data.aws_iam_policy_document.lambda_assume_role.json}"
 }
 
-resource "aws_iam_role_policy_attachment" "s3_full_access_attach" {
+resource "aws_iam_role_policy_attachment" "s3_attach" {
+  for_each = toset([
+    "arn:aws:iam::aws:policy/AmazonS3FullAccess",
+    "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+  ])
   role = "${aws_iam_role.lambda_iam_role.name}"
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+  policy_arn = each.key
 }
