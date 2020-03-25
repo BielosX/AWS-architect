93adf314be0f88033ff558e55304ad4bd1a5fc04
Mateusz Biel <matbiel@gmail.com>
2020-03-25 10:39:15 +0100

    update security group

--- serverless/terraform/lambda/main.tf
+++ serverless/terraform/lambda/main.tf
@@ -27,16 +27,30 @@ resource "aws_security_group" "lambda_security_group" {
   vpc_id = var.vpc_id
 
   ingress {
-    from_port = 443
-    to_port = 443
+    from_port = 0
+    to_port = 65535
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = 80
-    to_port = 80
+    from_port = 0
+    to_port = 65535
+    protocol = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 65535
     protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 65535
+    protocol = "udp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
