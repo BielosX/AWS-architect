d4e803cd73688c9d70b1bbcd009c64c6927aa3fd
Mateusz Biel <matbiel@gmail.com>
2020-08-15 22:56:03 +0200

    update

--- app/lambda_sns/infra/main.tf
+++ app/lambda_sns/infra/main.tf
@@ -6,6 +6,10 @@ resource "aws_s3_bucket" "lambda_bucket" {
   bucket = var.bucket_name
 }
 
+resource "aws_s3_bucket" "lambda_output_bucket" {
+  bucket = "lambda-output-bucket-${var.aws_region}"
+}
+
 resource "aws_s3_bucket_object" "initial_code" {
   bucket = aws_s3_bucket.lambda_bucket.id
   key = var.jar_file_name
@@ -27,6 +31,20 @@ resource "aws_iam_role" "lambda_iam_role" {
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
 }
 
+resource "aws_iam_role_policy_attachment" "attach_s3_full_access" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+  role = aws_iam_role.lambda_iam_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "attach_basic_execution" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role = aws_iam_role.lambda_iam_role.name
+}
+
+resource "aws_sns_topic" "lambda_trigger_topic" {
+  name = "lambda-trigger-topic-${var.aws_region}"
+}
+
 resource "aws_lambda_function" "lambda_sns" {
   depends_on = [aws_s3_bucket_object.initial_code]
   function_name = var.lambda_name
@@ -35,4 +53,25 @@ resource "aws_lambda_function" "lambda_sns" {
   runtime = "java11"
   s3_bucket = aws_s3_bucket.lambda_bucket.id
   s3_key = var.jar_file_name
+  timeout = 120
+  memory_size = 512
+  environment {
+    variables = {
+      BUCKET_NAME = aws_s3_bucket.lambda_output_bucket.id
+      REGION = var.aws_region
+    }
+  }
+}
+
+resource "aws_sns_topic_subscription" "lambda_topic_sub" {
+  endpoint = aws_lambda_function.lambda_sns.arn
+  protocol = "lambda"
+  topic_arn = aws_sns_topic.lambda_trigger_topic.arn
+}
+
+resource "aws_lambda_permission" "allow_sns_to_invoke_lambda" {
+  statement_id = "AllowExecutionFromSNS"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda_sns.function_name
+  principal = "sns.amazonaws.com"
 }
--- app/lambda_sns/src/main/java/Handler.java
+++ app/lambda_sns/src/main/java/Handler.java
@@ -1,10 +1,27 @@
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SNSEvent;
+import com.amazonaws.services.lambda.runtime.events.SNSEvent.SNSRecord;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+import java.util.List;
+
+import static java.util.UUID.randomUUID;
 
 public class Handler implements RequestHandler<SNSEvent, String> {
     @Override
     public String handleRequest(SNSEvent snsEvent, Context context) {
+        String bucketName = System.getenv("BUCKET_NAME");
+        String region = System.getenv("REGION");
+        AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(region).build();
+        List<SNSRecord> records = snsEvent.getRecords();
+        System.out.println("Received " + records.size() + " records");
+        records.forEach(snsRecord -> {
+            String message = snsRecord.getSNS().getMessage();
+            long timestamp = snsRecord.getSNS().getTimestamp().getMillis();
+            s3.putObject(bucketName, timestamp + "-" + randomUUID().toString() + ".txt", message);
+        });
         return "200 OK";
     }
 }
