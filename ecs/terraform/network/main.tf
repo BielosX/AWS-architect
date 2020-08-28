55bdee01a2fe732d29b109244f27fd9df73895a9
Mateusz Biel <matbiel@gmail.com>
2020-08-28 20:21:46 +0200

    update

--- ecs/terraform/network/main.tf
+++ ecs/terraform/network/main.tf
@@ -5,12 +5,23 @@ resource "aws_vpc" "cluster_vpc" {
   }
 }
 
+data "aws_availability_zones" "available_zones" {
+  state = "available"
+}
+
+locals {
+  subnets_cidr = ["10.0.1.0/24", "10.0.2.0/24"]
+  zone_names = data.aws_availability_zones.available_zones.names
+  number_of_zones = length(local.zone_names)
+}
+
 resource "aws_subnet" "private_subnets" {
-  for_each = toset(["10.0.1.0/24", "10.0.2.0/24"])
+  for_each = toset(local.subnets_cidr)
   vpc_id = aws_vpc.cluster_vpc.id
   tags = {
     Name = var.deployment_tag
   }
   cidr_block = each.value
   map_public_ip_on_launch = false
+  availability_zone = local.zone_names[index(local.subnets_cidr, each.value) % local.number_of_zones]
 }
