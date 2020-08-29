5d449f682fca90ded5361dde59a19d083b18ff15
Mateusz Biel <matbiel@gmail.com>
2020-08-29 10:47:47 +0200

    update

--- ecs/terraform/cluster/main.tf
+++ ecs/terraform/cluster/main.tf
@@ -36,6 +36,43 @@ data "aws_ssm_parameter" "recommended_ami" {
   name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id"
 }
 
+data "aws_iam_policy_document" "ec2_cluster_role_assume" {
+  statement {
+    effect = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      identifiers = ["ec2.amazonaws.com"]
+      type = "Service"
+    }
+  }
+}
+
+resource "aws_iam_role" "ec2_cluster_role" {
+  assume_role_policy = data.aws_iam_policy_document.ec2_cluster_role_assume.json
+}
+
+data "aws_iam_policy_document" "create_log_group_document" {
+  statement {
+    effect = "Allow"
+    actions = ["logs:CreateLogGroup"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "create_log_group_policy" {
+  policy = data.aws_iam_policy_document.create_log_group_document.json
+  role = aws_iam_role.ec2_cluster_role.id
+}
+
+resource "aws_iam_role_policy_attachment" "attach_container_service_fo_ec2" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+  role = aws_iam_role.ec2_cluster_role.id
+}
+
+resource "aws_iam_instance_profile" "ec2_cluster_profile" {
+  role = aws_iam_role.ec2_cluster_role.name
+}
+
 resource "aws_launch_template" "cluster_ec2_launch_template" {
   depends_on = [aws_ecs_cluster.cluster]
   tags = {
@@ -54,6 +91,9 @@ resource "aws_launch_template" "cluster_ec2_launch_template" {
   image_id = data.aws_ssm_parameter.recommended_ami.value
   vpc_security_group_ids = [aws_security_group.cluster_security_group.id]
   key_name = var.key_pair
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ec2_cluster_profile.name
+  }
 }
 
 resource "aws_autoscaling_group" "cluster_auto_scaling_group" {
