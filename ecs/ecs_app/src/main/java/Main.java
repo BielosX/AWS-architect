4154155b545d0728db807b1f0665e3a15a92a5b9
Mateusz Biel <matbiel@gmail.com>
2020-08-31 00:23:07 +0200

    update

--- ecs/ecs_app/src/main/java/Main.java
+++ ecs/ecs_app/src/main/java/Main.java
@@ -1,5 +1,8 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello");
+    public static void main(String[] args) throws InterruptedException {
+        do {
+            System.out.println("Hello");
+            Thread.sleep(20000);
+        } while (true);
     }
 }
