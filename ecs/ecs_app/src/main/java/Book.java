343a6bc6d22104955813a000f8b6d293815430ef
Mateusz Biel <matbiel@gmail.com>
2020-08-31 18:07:45 +0200

    use docker-compose

---
+++ ecs/ecs_app/src/main/java/Book.java
@@ -0,0 +1,8 @@
+import lombok.Value;
+
+@Value
+public class Book {
+    int id;
+    String firstName;
+    String lastName;
+}
--- ecs/ecs_app/src/main/java/Main.java
+++ ecs/ecs_app/src/main/java/Main.java
@@ -1,8 +1,21 @@
+import com.google.gson.Gson;
+import com.zaxxer.hikari.HikariDataSource;
+import org.jooq.DSLContext;
+import org.jooq.impl.DSL;
+import spark.Spark;
+
+import static org.jooq.SQLDialect.POSTGRES;
+import static org.jooq.codegen.ecs.app.tables.Books.BOOKS;
+
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        do {
-            System.out.println("Hello");
-            Thread.sleep(20000);
-        } while (true);
+    public static void main(String[] args) {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl("jdbc:postgresql://localhost:5432/appdb");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+        DSLContext context = DSL.using(dataSource, POSTGRES);
+        Gson gson = new Gson();
+
+        Spark.get("/books", (req, res) -> gson.toJson(context.selectFrom(BOOKS).fetchInto(Book.class)));
     }
 }
